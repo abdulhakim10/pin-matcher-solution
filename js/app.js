4ea1d4b100516823d5a4dc42603d840c2abe1b39
abdulhakim10 <s.abdulhakim10@gmail.com>
2022-08-20 21:11:10 +0600

    event handler add and show 4 digit pin code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('added')
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        // console.log('4 digit not found', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+    const pin = getPin();
+
+    // display pin
+    const pinField = document.getElementById('display-pin');
+    pinField.value = pin;
+
+    console.log(pin);
+})
